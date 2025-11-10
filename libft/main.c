a1fc060fbf64ce449880a6706fa715e3fff0312c
lennonmuller <lennon-muller@hotmail.com>
2025-11-10 14:08:32 +0000

    lists libft + corrections on makefile and libft.h

--- libft/main.c
+++ libft/main.c
@@ -10,7 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include "libft.h"
 
 char transform(unsigned int i, char c);
