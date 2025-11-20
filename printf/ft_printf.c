5cf36ac91fa98aa8fac4a230ceea9197457078ad
Lennon Lionel Muler Francisco Dos Santos <lennon-muller@hotmail.com>
2025-11-20 14:55:04 +0000

    mudanca geral printf

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,107 +6,108 @@
 /*   By: lmuler-f <lmuler-f@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 13:02:17 by lmuler-f          #+#    #+#             */
-/*   Updated: 2025/11/12 13:02:17 by lmuler-f         ###   ########.fr       */
+/*   Updated: 2025/11/20 12:36:52 by lmuler-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdarg.h>
 #include "ft_printf.h"
+#include <stdarg.h>
+#include <stdio.h>
 
-static void parse_flags(const char **format, t_flags *flags);
-static t_flags init_flags(void);
+static void		parse_flags(const char **format, t_flags *flags);
+static t_flags	init_flags(void);
 
-static int  parse_and_print(const char **format, va_list args)
+static int	parse_and_print(const char **format, va_list args)
 {
-    t_flags flags;
-    int     count;
+	t_flags	flags;
+	int		count;
+	char	spec;
 
-    (*format)++;
-    flags = init_flags();
-    parse_flags(format, &flags);
-    char spec = **format;
-    count = 0;
-    if (spec == 'c')
-        count += print_char(args);
-    else if (spec == 's')
-        count += print_string(args);
-    else if (spec == 'p')
-        count += print_pointer(args);
-    else if (spec == 'd' || spec == 'i')
-        count += print_int(args);
-    else if (spec == 'u')
-        count += print_unsigned(args);
-    else if (spec == 'x' || spec == 'X')
-        count += print_hex(args, spec);
-    else if (spec == '%')
-        count += write(1, "%", 1);
-    (*format)++;
-    return (count);
+	(*format)++;
+	flags = init_flags();
+	parse_flags(format, &flags);
+	spec = **format;
+	count = 0;
+	if (spec == 'c')
+		count += print_char(args);
+	else if (spec == 's')
+		count += print_string(args);
+	else if (spec == 'p')
+		count += print_pointer(args);
+	else if (spec == 'd' || spec == 'i')
+		count += print_int(args);
+	else if (spec == 'u')
+		count += print_unsigned(args);
+	else if (spec == 'x' || spec == 'X')
+		count += print_hex(args, spec);
+	else if (spec == '%')
+		count += write(1, "%", 1);
+	(*format)++;
+	return (count);
 }
 
-int ft_printf(const char *format, ...)
+int	ft_printf(const char *format, ...)
 {
-    va_list ap;
-    int     count;
+	va_list	ap;
+	int		count;
 
-    count = 0;
-    va_start(ap, format);
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            count += parse_and_print(&format, ap);
-        }
-        else
-        {
-            ft_putchar_fd(*format, 1);
-            count++;
-            format++;
-        }
-    }
-    va_end(ap);
-    return (count);
+	count = 0;
+	va_start(ap, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			count += parse_and_print(&format, ap);
+		}
+		else
+		{
+			ft_putchar_fd(*format, 1);
+			count++;
+			format++;
+		}
+	}
+	va_end(ap);
+	return (count);
 }
 
-static t_flags init_flags(void)
+static t_flags	init_flags(void)
 {
-    t_flags flags;
+	t_flags	flags;
 
-    flags.minus = 0;
-    flags.zero = 0;
-    flags.width = 0;
-    flags.dot = 0;
-    flags.precision = 0;
-    flags.hash = 0;
-    flags.space = 0;
-    flags.plus = 0;
-    return (flags);
+	flags.minus = 0;
+	flags.zero = 0;
+	flags.width = 0;
+	flags.dot = 0;
+	flags.precision = 0;
+	flags.hash = 0;
+	flags.space = 0;
+	flags.plus = 0;
+	return (flags);
 }
 
-static void parse_flags(const char **format, t_flags *flags)
+static void	parse_flags(const char **format, t_flags *flags)
 {
-    while (**format == '-' || **format == '0')
-    {
-        if (**format == '-')
-            flags->minus = 1;
-        else if (**format == '0')
-            flags->zero = 1;
-        (*format)++;
-    }
-    while (**format >= '0' && **format <= '9')
-    {
-        flags->width = flags->width * 10 + (**format - '0');
-        (*format)++;
-    }
-    if (**format == '.')
-    {
-        flags->dot = 1;
-        (*format)++;
-        while (**format >= '0' && **format <= '9')
-        {
-            flags->precision = flags->precision * 10 + (**format - '0');
-            (*format)++;
-        }
-    }
+	while (**format == '-' || **format == '0')
+	{
+		if (**format == '-')
+			flags->minus = 1;
+		else if (**format == '0')
+			flags->zero = 1;
+		(*format)++;
+	}
+	while (**format >= '0' && **format <= '9')
+	{
+		flags->width = flags->width * 10 + (**format - '0');
+		(*format)++;
+	}
+	if (**format == '.')
+	{
+		flags->dot = 1;
+		(*format)++;
+		while (**format >= '0' && **format <= '9')
+		{
+			flags->precision = flags->precision * 10 + (**format - '0');
+			(*format)++;
+		}
+	}
 }
--- printf/includes/ft_printf.h
+++ printf/includes/ft_printf.h
@@ -6,38 +6,38 @@
 /*   By: lmuler-f <lmuler-f@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 11:10:27 by lmuler-f          #+#    #+#             */
-/*   Updated: 2025/11/17 11:10:27 by lmuler-f         ###   ########.fr       */
+/*   Updated: 2025/11/20 14:18:32 by lmuler-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include "libft/libft.h"
+# include "libft.h"
 # include <stdarg.h>
 # include <stdio.h>
 # include <limits.h>
 # include <unistd.h>
 
-int ft_printf(const char *format, ...);
-int ft_putnbr_base(unsigned long long nbr, char *base);
-int print_string(va_list args);
+int	ft_printf(const char *format, ...);
+int	ft_putnbr_base(unsigned long long nbr, char *base);
+int	print_string(va_list args);
 int	print_int(va_list args);
-int print_char(va_list args);
-int print_pointer(va_list args);
-int print_unsigned(va_list args);
-int print_hex(va_list args, char spec);
+int	print_char(va_list args);
+int	print_pointer(va_list args);
+int	print_unsigned(va_list args);
+int	print_hex(va_list args, char spec);
 
 typedef struct s_format_flags
 {
-	int		minus;  // Flag '-' (alinhamento à esquerda)
-	int		zero;   // Flag '0' (padding com zeros)
-	int		width;  // Largura mínima do campo
-	int		dot;    // Flag '.' (precisão)
-	int		precision; // Valor da precisão
-	int		hash;   // Flag '#' (formato alternativo 0x)
-	int		space;  // Flag ' ' (espaço para positivos)
-	int		plus;   // Flag '+' (sinal para positivos)
+	int		minus;
+	int		zero;
+	int		width;
+	int		dot;
+	int		precision;
+	int		hash;
+	int		space;
+	int		plus;
 }	t_flags;
 
 #endif
